adc1411d16c44d6ed56ad3129f4230a9e8260115
Emre <emrekurtt1655@gmail.com>
2020-11-11 00:05:47 +0100

    build: components and assets in project created

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {header} from './styles';
+
+const Header = (props) => {
+  return (
+    <View style={header.container}>
+      {
+        // ...
+      }
+    </View>
+  );
+};
+
+export {Header};
