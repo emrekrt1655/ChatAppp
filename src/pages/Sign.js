bfee116350f2310e5a9ea5157f86d4e1edb47dd6
Emre <emrekurtt1655@gmail.com>
2020-11-11 02:07:26 +0100

    add: create an e-mail added

--- src/pages/Sign.js
+++ src/pages/Sign.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
   SafeAreaView,
   Dimensions,
@@ -7,12 +7,34 @@ import {
   Image,
   ScrollView,
   KeyboardAvoidingView,
+  Alert,
 } from 'react-native';
 
 import {authStyle} from './styles';
 import {Input, Button} from '../components';
+import auth from '@react-native-firebase/auth'
 
 const Sign = (props) => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRepeat, setPasswordRepeat] = useState('') // daha az state olabilir mi?
+
+  const sign = async () => { // geçerli email ve doğru değer kontrolü (REGEX)
+    if (password === passwordRepeat){
+      try{
+      await auth().createUserWithEmailAndPassword(email,password);
+      props.navigation.goBack()
+      }
+      catch(error){
+        Alert.alert('ClarusChat', 'An error occured');
+      }
+    }
+    else{
+      Alert.alert('ClarusChat', 'Passwords do not match')
+    }
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#cfd8dc'}}>
@@ -32,21 +54,25 @@ const Sign = (props) => {
                   placeholder: 'Type your e-mail address..',
                   keyboardType: 'email-address'
                 }}
+                onType={value => setEmail(value)}
               />
               <Input
                 inputProps={{
                   placeholder: 'Enter your password..',
-                  secureTypeEntry: true
+                  secureTextEntry: true
                 }}
+                onType={value => setPassword(value)}
               />
               <Input
                 inputProps={{
                   placeholder: 'Enter your password again..',
-                  ksecureTypeEntry: true
+                  secureTextEntry: true
                 }}
+                onType={value => setPasswordRepeat(value)}
               />
               <Button
                 title='Create an account'
+                onPress = {()=>sign()}
               />
               <Button
                 title='Cancel'
